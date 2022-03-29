299892ba02a7f7e6e5fa439db9e1aa9d66df24d8
rsek <hello@rsek.ca>
2022-03-28 21:25:00 -0700

    upgrade discordx to take advantage of fix

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 
-import { IntentsBitField } from "discord.js";
+import { ApplicationCommand, Collection, IntentsBitField } from "discord.js";
 import { dirname, importx } from "@discordx/importer";
 import { ArgsOf, Client, Discord, On, Once } from "discordx";
 import dotenv from "dotenv";
@@ -29,8 +29,6 @@ export abstract class Bot {
       IntentsBitField.Flags.DirectMessages
     ];
 
-    // const botGuilds = process.env.NODE_ENV === "development" && process.env.GUILD ? [process.env.GUILD] : undefined;
-
     this._client = new Client({
       intents,
       silent: false,
@@ -47,25 +45,14 @@ export abstract class Bot {
     if (process.env.NODE_ENV === "development" && process.env.GUILD) {
       Bot.client.botGuilds = [process.env.GUILD];
     }
-    // else {
-    //   Bot.client.botGuilds = [(client) => client.guilds.cache.map((guild) => guild.id)];
-    // }
 
-    // if (process.env.NODE_ENV === "production") {
-    //   console.log("Initializing global application commands...");
-    //   await Bot.client.initGlobalApplicationCommands({ log: true });
-    // } else  {
     console.log("Initializing application commands...");
     await Bot.client.initApplicationCommands({ guild: { log: true }, global: { log: true } });
-    // }
 
     console.log("Initializing application command permissions...");
     await Bot.client.initApplicationPermissions(true);
 
-    const commands = await Bot.client.fetchApplicationCommands();
-
-    console.log("Bot has the following commands registered:");
-    console.log(commands?.values());
+    const commands = await Bot.client.fetchApplicationCommands() as Collection<string, ApplicationCommand>;
 
     console.log(`Bot started in ${process.env.NODE_ENV ?? "[ERROR]"} mode.`);
 
@@ -73,26 +60,6 @@ export abstract class Bot {
   }
   @On("interactionCreate")
   async onInteractionCreate([interaction]: ArgsOf<"interactionCreate">) {
-    // console.log("[Bot.client.botGuilds]", Bot.client.botGuilds);
-    // const logItems = ["[interactionCreate]"];
-
-    // switch (interaction.type) {
-    //   case InteractionType.ApplicationCommand: {
-    //     logItems.push("Application Command", (interaction as CommandInteraction).commandName);
-    //     break;}
-    //   case InteractionType.MessageComponent: {
-    //     logItems.push("Message Component",
-    //       ComponentType[(interaction as MessageComponentInteraction).componentType],
-    //       (interaction as MessageComponentInteraction).customId);
-    //     if ((interaction as MessageComponentInteraction).componentType === ComponentType.SelectMenu) {
-    //       logItems.push((interaction as SelectMenuInteraction).valueOf(),...(interaction as SelectMenuInteraction).values);
-    //     }
-    //     break;
-    //   }
-    //   default:
-    //     break;
-    // }
-    // console.log(...logItems);
     await Bot.client.executeInteraction(interaction, true);
   }
 }
