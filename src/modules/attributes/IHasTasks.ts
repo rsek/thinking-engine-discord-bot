1c7d0ce23b23cef96b08c3c8e4813caed360741d
rsek <hello@rsek.ca>
2022-03-29 14:48:55 -0700

    rm orphans

--- src/modules/attributes/IHasTasks.ts
+++
@@ -1,6 +0,0 @@
-import type { BotTask } from "../tasks/BotTask.js";
-import type { packParams } from "../tasks/packParams.js";
-
-export default interface IHasTask<T extends BotTask> {
-  getTaskString<T>(...args: any[]): ReturnType<typeof packParams>
-}
